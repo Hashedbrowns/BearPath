0ab14cc8dbec576d6e4040b908f172bfb2620fd6
Ashwin <ashwinshreekumar123@gmail.com>
2023-04-29 13:51:04 -0600

    Debug=True

--- uofaDirections/settings.py
+++ uofaDirections/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_g99&&x&(te4t#okf!vxwzm2gxednw5v#=#c@h2su7^d)+^&0='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'uofadirection-production.up.railway.app']
 
